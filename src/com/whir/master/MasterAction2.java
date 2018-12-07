eba938fac759b6d1ca55906713b173e9d7c64f5d
lvjian <1349256352@qq.com>
2018-12-07 16:32:24 +0800

    添加MasterAction2.java

---
+++ src/com/whir/master/MasterAction2.java
@@ -0,0 +1,14 @@
+package com.whir.master;
+
+public class MasterAction {
+	public static void main(String[] args) {
+		System.out.println("master");
+	}
+	
+	public void masterA(){
+		System.out.println("masterA");
+	}
+	public void masterB(){
+		System.out.println("masterB");
+	}
+}
